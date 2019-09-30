d49b67f83325bbabe70697933775a877d60647b2
Nikita Krutoy <nikitakrutoy@gmail.com>
2019-10-01 01:12:54 +0300

    format fixes

--- metrics/worker_dashboard.py
+++ metrics/worker_dashboard.py
@@ -2,7 +2,7 @@ import plotly.graph_objs as go
 
 def loss_time(vis, state):
     vis.line(state["loss"][-1:], [sum(state["compute_time"])], win="loss_time", 
-        name=f"worker_{state['id']}", update="append",
+        name=f"worker_{state['worker_id']}", update="append",
         opts=dict(
                 showlegend=True,
                 xlabel="Time, s",
@@ -12,7 +12,7 @@ def loss_time(vis, state):
 
 def loss_step(vis, state):
     vis.line(state["loss"][-1:], [state["step_num"]], win="loss_step", 
-        name=f"worker_{state['id']}", update="append",
+        name=f"worker_{state['worker_id']}", update="append",
         opts=dict(
                 showlegend=True,
                 xlabel="Step",
@@ -22,7 +22,7 @@ def loss_step(vis, state):
 
 def time_step(vis, state):
     vis.line(state["compute_time"][-1:], [state["step_num"]], win="time_step", 
-        name=f"worker_{state['id']}", update="append",
+        name=f"worker_{state['worker_id']}", update="append",
         opts=dict(
                 showlegend=True,
                 xlabel="Step",
@@ -34,7 +34,7 @@ def time_hist(vis, state):
     if len(state["compute_time"]) > 1:
         vis.histogram(
             state["compute_time"], 
-            env=state['id'],
+            env=state['worker_id'],
             win="step_time",
             opts=dict(
                 title=f"step_time"
@@ -45,7 +45,7 @@ def download_time(vis, state):
     if len(state["download_time"]) > 1:
         vis.histogram(
             state["download_time"], 
-            env=state['id'],
+            env=state['worker_id'],
             win="download_time",
             opts=dict(
                 title=f"download_time"
@@ -54,7 +54,7 @@ def download_time(vis, state):
         )
 
     vis.line(state["download_time"][-1:], [sum(state["compute_time"])], win="downloadtime_step", 
-        name=f"worker_{state['id']}", update="append",
+        name=f"worker_{state['worker_id']}", update="append",
         opts=dict(
                 showlegend=True,
                 xlabel="Time",
